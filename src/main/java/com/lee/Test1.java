dd74dee24cc943e16cfa0f0c5153b7390880647f
BlueLeer <251668577@qq.com>
2019-04-22 23:15:27 +0800

    test分支提交Test代码

---
+++ src/main/java/com/lee/Test1.java
@@ -0,0 +1,14 @@
+package com.lee;
+
+/**
+ * Created with : IntelliJ IDEA
+ * User : KingIsHappy
+ * Date : 2019/4/22
+ * Time : 23:15
+ * Description :
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
